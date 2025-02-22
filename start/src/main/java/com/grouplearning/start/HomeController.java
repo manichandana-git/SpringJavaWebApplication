2ddf2ae4ab75cdc34a3cc1b17e7fa7ce1b926b86
manichandana-git <manichandana.talluri.usjobs@gmail.com>
2025-02-21 21:02:11 -0600

    initialization of project structure and downloads of proper technologies and frameworks

---
+++ start/src/main/java/com/grouplearning/start/HomeController.java
@@ -0,0 +1,15 @@
+package com.grouplearning.start;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Welcome to My Spring Boot Application!";
+    }
+}
